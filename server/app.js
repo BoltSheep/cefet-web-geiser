fec4afd4a9f1c6b96d6ead8555905279edfb3f18
BoltSheep <thidampe@gmail.com>
2022-01-25 23:56:42 -0300

    Atividades Geiser
    Exercicio 1, 2
    exerciocio 3 50%

--- server/app.js
+++ server/app.js
@@ -4,6 +4,13 @@
 // variáveis globais deste módulo
 const PORT = 3000
 const db = {}
+const fs = require('fs')
+
+var express = require('express'),
+    app = express();
+
+const getFile = (path) => JSON.parse(fs.readFileSync(path, 'utf-8'))
+
 
 
 // carregar "banco de dados" (data/jogadores.json e data/jogosPorJogador.json)
@@ -18,6 +25,9 @@ const db = {}
 //app.set('views', '???caminho-ate-pasta???');
 // dica: 2 linhas
 
+app.set('view engine','hbs')
+app.set('views', 'server/views')
+
 
 // EXERCÍCIO 2
 // definir rota para página inicial --> renderizar a view index, usando os
@@ -27,17 +37,45 @@ const db = {}
 
 
 
+app.get('/', function (req, res) {
+    const data = getFile('server/data/jogadores.json')
+    console.log('miojo + arroz = ', data)
+    res.render('index', data)
+})
+
 // EXERCÍCIO 3
 // definir rota para página de detalhes de um jogador --> renderizar a view
 // jogador, usando os dados do banco de dados "data/jogadores.json" e
 // "data/jogosPorJogador.json", assim como alguns campos calculados
 // dica: o handler desta função pode chegar a ter ~15 linhas de código
 
+app.get('/jogador/:id/', function(req, res){
+    const players = getFile('server/data/jogadores.json')
+    const games = getFile('server/data/jogosPorJogador.json')[req.params.id]
+
+    const player = players.players.find((j) => req.params.id === j.steamid)
+    
+    console.log(player)
+
+    games.not_played_count = games.games.filter((g) => !g.playtime_forever).length || 0
+
+    games.played_the_most = games.games.reduce((acc, r) => acc.playtime_forever > r.playtime_forever ? acc : r)
+
+    res.render('jogador', {player, games})
+
+})
+
+
+
 
 // EXERCÍCIO 1
 // configurar para servir os arquivos estáticos da pasta "client"
 // dica: 1 linha de código
 
+app.use(express.static("./client"))
 
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`)
+  })
 // abrir servidor na porta 3000 (constante PORT)
 // dica: 1-3 linhas de código
